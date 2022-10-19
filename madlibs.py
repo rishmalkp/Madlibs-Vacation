236962cdab466827b1b40bc31d1c43dc26a10c12
Rishmal kp <57453280+rishmalkp@users.noreply.github.com>
2022-10-19 22:08:35 +0400

    Create madlibs.py
    Have fun with Vacation Fun Mad Libs

---
+++ madlibs.py
@@ -0,0 +1,36 @@
+
+
+
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+noun1 = input("Noun: ")
+noun2 = input("Noun: ")
+game = input("Game: ")
+plural_noun1 = input("Plural Noun: ")
+verb_ending_ing1 = input("Verb ending in 'ING': ")
+verb_ending_ing2 = input("verb ending in 'ING': ")
+plural_noun2 = input("Plural Noun: ")
+verb_ending_ing3 = input("verb ending in 'ING': ")
+noun3 = input("Noun: ")
+plant = input("Plant: ")
+part_of_body = input("Part of the body: ")
+a_place = input("A Place: ")
+verb_ending_ing4 = input("verb ending in 'ING': ")
+adj3 = input("Adjective: ")
+number = input("Number: ")
+plural_noun3 = input("Plural Noun: ")
+
+
+madlib = f"A vaccation is when you take a trip to some {adj1} place \
+with your {adj2} family. Usually you go to some place that is near \
+a/an {noun1} or up a/an {noun2}. A good vacation place is one where \
+you can ride {plural_noun1} or play {game} or go hunting for {plural_noun2}.\
+I like to spend my time {verb_ending_ing1} or {verb_ending_ing2}. When \
+parents go on vacation, they play golf, and mothers sit around {verb_ending_ing3}.\
+ Last summer, my little brother fell in a/an {noun3} and got poison {plant}\
+all over his {part_of_body}. My family is going to go to the  {a_place},\
+and I will practice {verb_ending_ing4}. Parents need vacations more than kids \
+because parents are always very {adj3} and because they have to work {number}\
+ hours every day all year making enough {plural_noun3} to pay for the vacation."
+
+print(madlib)
